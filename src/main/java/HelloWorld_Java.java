85f59aa674ce28f64153ba24bfda2f5be0205acb
MohamedOutrgua <105601074+MohamedOutrgua@users.noreply.github.com>
2023-02-07 20:11:32 -0500

    Up to date now

--- src/main/java/HelloWorld_Java.java
+++ src/main/java/HelloWorld_Java.java
@@ -1,5 +1,6 @@
 public class HelloWorld_Java {
     public static void main(String[] args) {
         System.out.println("Hello this is my first Java project");
+        System.out.println("Hello my name is bla bla bla bla");
     }
 }
