2054120cac989d255e2a788395e94a929aa76c2f
MohamedOutrgua <105601074+MohamedOutrgua@users.noreply.github.com>
2023-02-07 20:35:08 -0500

    Selenium Test

--- src/main/java/HelloWorld_Java.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld_Java {
-    public static void main(String[] args) {
-        System.o.println("Hello this is my first Java project");
-        System.ouprintln("Hello my name is bla bla bla bla");
-        System.out.prn("I have a car");
-
-    }
-}
